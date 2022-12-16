42ef2bd97e9720e267c7367ccb7c46fc53da1f8a
drhazel751 <112452033+drhazel751@users.noreply.github.com>
2022-12-15 20:57:08 -0700

    Update FinalJs.js

--- Final/FinalJs.js
+++ Final/FinalJs.js
@@ -6,8 +6,8 @@ var Player1Score = 0
 var Player2Score = 0
 var a = 65
 var d = 68
-var Paddle1X = 205
-var Paddle2X = 205
+var Paddle1X = 195
+var Paddle2X = 195
 
 function setup() {
   createCanvas(500, 600);
@@ -18,6 +18,8 @@ function draw(){
   fill('white');
   rect (0, 298, 500, 2);
   circle (250, 299, 8)
+  text ('<-- and -->', 10, 582, 70)
+  text ('A and D', 430, 10, 50)
   
   move();
   display();
@@ -27,33 +29,33 @@ function draw(){
   
   //Paddle1
   fill('blue');
-  rect(Paddle1X, 575, 90, 15);
-  if(keyIsDown(a)){
+  rect(Paddle1X, 575, 100, 25);
+  if(keyIsDown(LEFT_ARROW)){
         if(Paddle1X > 5)
     {
-        Paddle1X -= 6;   
+        Paddle1X -= 8;   
     }
       }
-  if(keyIsDown(d)){
+  if(keyIsDown(RIGHT_ARROW)){
         if(Paddle1X < 410)
     {
-        Paddle1X += 6;   
+        Paddle1X += 8;   
     }
       }
 
     // Paddle2
     fill('red');
-  rect(Paddle2X, 15, 90, 15);
-  if(keyIsDown(LEFT_ARROW)){
-        if(Paddle2X > 5)
+  rect(Paddle2X, 0, 100, 25);
+  if(keyIsDown(a)){
+        if(Paddle2X > 0)
     {
-        Paddle2X -= 6;   
+        Paddle2X -= 8;   
     }
       }
-  if(keyIsDown(RIGHT_ARROW)){
+  if(keyIsDown(d)){
         if(Paddle2X < 410)
     {
-        Paddle2X += 6;   
+        Paddle2X += 8;   
     }
       }
   
@@ -67,18 +69,22 @@ function draw(){
 function bounce() {
 
   if (xBall < 10 ||
-    xBall > 500 - 10) {
+    xBall > 500 - 15) {
     xSpeed *= -1;
   }
   if (yBall < 10 ||
-    yBall > 600 - 10) {
+    yBall > 600 - 15) {
     ySpeed *= -1;
     }
-  if (yBall > 590) {
+  if (yBall > 585) {
     Player2Score++;
+    yBall = 300;
+    xBall = 250
   }
     if (yBall < 10) {
     Player1Score++;
+    yBall = 300;
+    xBall = 250;
   }
 }
 
@@ -101,7 +107,7 @@ function paddle() {
   if ((xBall > Paddle1X &&
       xBall < Paddle1X + 90) &&
     (yBall + 10 >= 575)) {
-    xSpeed *= -1;
+    xSpeed *= 1 || -1;
     ySpeed *= -1;
 
   }
@@ -111,8 +117,8 @@ function paddle() {
 function paddle2() {
   if ((xBall > Paddle2X &&
       xBall < Paddle2X + 90) &&
-    (yBall - 15 <= 15)) {
-    xSpeed *= -1;
+    (yBall - 10 <= 20)) {
+    xSpeed *= 1|| -1;
     ySpeed *= -1;
 
   }
